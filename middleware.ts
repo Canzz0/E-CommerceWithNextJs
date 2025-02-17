1edbd4a7b38c051944bcab9a9a7b34dc4fe428eb
Can <mustafacann43@gmail.com>
2025-02-17 20:39:33 +0300

    middleware fixed

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get('token')
+  const token = request.cookies.get('Authorization')
   
   if (request.nextUrl.pathname === '/login' && token) {
     return NextResponse.redirect(new URL('/', request.url))
